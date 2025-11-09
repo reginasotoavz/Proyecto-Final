9ab96a88055f8e256623a6e314c8752da2679ee9
Regina Soto <reginasotoavz@ciencias.unam.mx>
2025-11-09 23:00:08 +0000

    Classes added:
    new file:   bin/Ayudante.class
    new file:   bin/Main.class
    new file:   bin/Profesor.class
    new file:   bin/Tarea.class
    new file:   bin/Usuario.class
    modified:   src/Main.java
    :wp

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 import modulos.Usuario;
-import modulos.Porfesor;
+import modulos.Profesor;
 import modulos.Ayudante;
 import modulos.Tarea;
 
